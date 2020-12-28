6a96b6145167701b06ce6c46890534d641d71525
HYKANTUS <66837193+HYKANTUS@users.noreply.github.com>
2020-12-28 23:12:44 +0400

    added comment for directories

--- run.bat
+++ run.bat
@@ -2,7 +2,10 @@
 :: Loopcount set to 450 as that seems to be close to the limit (you may change it from 450-500 at your own risk of not being able to use the InstaBot library)
 set loopcount=450
 :loop
+
+:: First quote for python.exe location                          | Second quote for python script location.
 "C:\Users\HP\AppData\Local\Programs\Python\Python39\python.exe" "C:\Users\HP\Documents\pythonProject\main.py"
+
 set /a loopcount=loopcount-1
 if %loopcount%==0 goto exitloop
 goto loop
