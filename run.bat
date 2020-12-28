e9dcd4f0f83467b9307c001a4a4bd60ddd276e25
HYKANTUS <66837193+HYKANTUS@users.noreply.github.com>
2020-12-28 10:56:20 +0400

    edited loopcount
    changed loopcount amount and added comment about loopcount

--- run.bat
+++ run.bat
@@ -1,5 +1,6 @@
 @echo on
-set loopcount=1000000
+:: Loopcount set to 450 as that seems to be close to the limit (you may change it from 450-500 at your own risk of not being able to use the InstaBot library)
+set loopcount=450
 :loop
 "C:\Users\HP\AppData\Local\Programs\Python\Python39\python.exe" "C:\Users\HP\Documents\pythonProject\main.py"
 set /a loopcount=loopcount-1
